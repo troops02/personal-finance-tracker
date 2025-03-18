2aeab2601a91e8573e9695c8b5fc3e5f559ec4a0
Lateef Omohagbon <omohagbon02@gmail.com>
2025-03-18 13:58:59 +0100

    element,styles & javaScript added

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,36 @@
 'use strict';
 
+const incomeSource = document.querySelector('.tracker__income-source');
+const incomeAmount = document.querySelector('.tracker__income-amount');
+const incomeInput = document.querySelector('.tracker__income-input');
+const entryBtn = document.querySelector('.entry-btn');
+
+let total = 0;
+const totalAmount = [];
+const createIncomeEl = function (income, amount) {
+  const cap = income.replace(income[0], income[0].toUpperCase());
+  const html = `
+    <div class="tracker__icome-input-log-entry">
+        <p class="income-text">${cap}</p>
+        <p class="income-salary">${amount}</p>
+    </div>`;
+
+  incomeInput.insertAdjacentHTML('beforeend', html);
+};
+
+entryBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  const income = incomeSource.value.trim();
+  const amount = +incomeAmount.value.trim();
+  if (!income & !amount) {
+    alert('Enter a valid income source and amount');
+    return;
+  }
+  createIncomeEl(income, amount);
+  incomeSource.value = '';
+  incomeAmount.value = '';
+  totalAmount.push(amount);
+  total += amount;
+  console.log(totalAmount);
+  console.log(total);
+});
