cbc2b5c9152b12f0dac9ec4f73fdfd465cfc6e6c
Lateef Omohagbon <omohagbon02@gmail.com>
2025-03-18 12:03:47 +0100

    corrected

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const saving = document.getElementById('#saving');
+const entry = document.querySelector('.entry-btn');
+
+entry.addEventListener('click', function () {
+  const value = +saving.value;
+  console.log(value);
+});
