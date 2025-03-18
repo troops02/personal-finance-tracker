e9693ce78385cb9c635752da0e0f250edd516b06
Lateef Omohagbon <omohagbon02@gmail.com>
2025-03-18 12:05:24 +0100

    removed

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,2 @@
 'use strict';
 
-const saving = document.getElementById('#saving');
-const entry = document.querySelector('.entry-btn');
-
-entry.addEventListener('click', function () {
-  const value = +saving.value;
-  console.log(value);
-});
