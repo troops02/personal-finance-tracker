20a64760fbe82e08d34a3a26612bcf7b7ad3e7f0
Lateef Omohagbon <omohagbon02@gmail.com>
2025-03-19 10:19:14 +0100

    chages made

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,18 @@
 const incomeSource = document.querySelector('.tracker__income-source');
 const incomeAmount = document.querySelector('.tracker__income-amount');
 const incomeInput = document.querySelector('.tracker__income-input');
-const entryBtn = document.querySelector('.entry-btn');
+const incomEntryBtn = document.querySelector('.entry-btn');
 const calcTotalAmount = document.querySelector('.total');
+
+const expenseSource = document.querySelector('.tracker__expense-source');
+const expenseAmount = document.querySelector('.tracker__expense-value');
+const expenseEntryBtn = document.querySelector('.expense__entery-btn');
+const expenseLog = document.querySelector('.tracker__expense-log');
 
 let total = 0;
 const totalAmount = [];
+const expensseAmount = [];
+
 const createIncomeEl = function (income, amount) {
   const cap = income.replace(income[0], income[0].toUpperCase());
   const html = `
@@ -19,21 +26,51 @@ const createIncomeEl = function (income, amount) {
   incomeInput.insertAdjacentHTML('beforeend', html);
 };
 
-entryBtn.addEventListener('click', function (e) {
+const createExpenseEl = function (expense, amount) {
+  const cap = expense.replace(expense[0], expense[0].toUpperCase());
+  const html = `
+  <div class="tracker__expense-entry">
+    <p>${cap}</p>
+    <p class="expense-log__salary">${amount}</p>
+  </div>`;
+  expenseLog.insertAdjacentHTML('beforeend', html);
+};
+
+incomEntryBtn.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const income = incomeSource.value.trim();
+  const income = incomeSource.value;
   const amount = +incomeAmount.value.trim();
 
   totalAmount.push(amount);
   total += amount;
+  console.log(total, totalAmount);
 
-  if (!income & !amount) {
-    alert('Enter a valid income source and amount');
+  if (!income || !amount) {
+    alert('Enter a valid input');
     return;
   }
   createIncomeEl(income, amount);
   incomeSource.value = '';
   incomeAmount.value = '';
   calcTotalAmount.textContent = `${total}`;
+});
+
+expenseEntryBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const expense = expenseSource.value;
+  const amount = +expenseAmount.value.trim();
+
+  expensseAmount.push(amount);
+  total -= amount;
+  console.log(expenseAmount,total);
+
+  if (!expense || !amount) {
+    alert('Enter a valid expense');
+    return;
+  }
+  createExpenseEl(expense, amount);
+  expenseSource.value = '';
+  expenseAmount.value = '';
 });
